4eff5cc83c3388a1b7d10a3e36b01b88076132ec
AlphansoTech <chaturj@alphansotech.com>
2024-12-04 13:27:31 +0530

    Initial Commit

---
+++ AlphansoSeat/AlphansoSeat.h
@@ -0,0 +1,18 @@
+//
+//  AlphansoSeat.h
+//  AlphansoSeat
+//
+//  Created by alphansotech100 on 04/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlphansoSeat.
+FOUNDATION_EXPORT double AlphansoSeatVersionNumber;
+
+//! Project version string for AlphansoSeat.
+FOUNDATION_EXPORT const unsigned char AlphansoSeatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlphansoSeat/PublicHeader.h>
+
+
